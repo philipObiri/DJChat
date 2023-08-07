80b75c0699257dee3e27767a7c4be36834f6c0ca
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-07 22:58:04 +0000

    feat: Adding the chat model to store chats in the database

---
+++ chatbot/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["-created_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} : {self.message}"
